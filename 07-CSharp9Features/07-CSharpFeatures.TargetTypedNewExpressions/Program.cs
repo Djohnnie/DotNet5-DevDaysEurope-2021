8555d79b9e2ae6aff99fb596a4814f300ce3ed2d
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2021-06-08 23:51:04 +0200

    Initial commit

---
+++ 07-CSharp9Features/07-CSharpFeatures.TargetTypedNewExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+Dictionary<string, string> dictionary1 = new Dictionary<string, string>();
+var dictionary2 = new Dictionary<string, string>();
+Dictionary<string, string> dictionary3 = new();
+
+Dictionary<string, List<Person>> dictionary4 = new()
+{
+    { "A", new() { new("1") { Age = 1 }, new("2") { Age = 2 } } },
+    { "B", new() { new("3") { Age = 3 }, new("4") { Age = 4 } } },
+    { "C", new() { new("5") { Age = 5 }, new("6") { Age = 6 } } }
+};
+
+DoSomething(new());
+
+void DoSomething(List<Person> p)
+{
+
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public byte Age { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
---
+++ 09-SourceGenerators/09-SourceGenerators.Demo/HttpClient.cs
@@ -0,0 +1,11 @@
+﻿using _09_SourceGenerators.Attributes;
+
+namespace _09_SourceGenerators.Demo
+{
+    [SwaggerClient(SwaggerDescriptor = 
+        "http://localhost:5000/swagger/v1/swagger.json")]
+    public partial class HttpClient
+    {
+
+    }
+}
